a8c4f25891e1a07889a084811a8165fb38ced0a9
EricPimenta <r.macedo7@gmail.com>
2021-09-17 23:32:11 -0300

    update

--- program.cs
+++ program.cs
@@ -1,1 +1,13 @@
-publicclassProduto{publicstringNome; publicdoublePreco; publicintQuantidade;publicProduto (stringnome, doublepreco, intquantidade){Nome = nome;Preco= preco;Quantidade = quantidade;}publicProduto (){}public doubleValorTotalEmEstoque() {returnPreco* Quantidade;}publicvoidAdicionarProduto(intquantidade) {Quantidade += quantidade;}publicvoidRemoverProdutos(intquantidade) {Quantidade = Quantidade -quantidade;}publicoverridestringToString() {returnNome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);}}
+public class Produto
+{
+    publicstringNome;
+     publicdoublePreco;
+      publicintQuantidade;
+      publicProduto (stringnome, doublepreco, intquantidade)
+      {
+          Nome = nome;Preco= preco;Quantidade = quantidade;
+          }
+          publicProduto ()
+          {
+              public doubleValorTotalEmEstoque() {returnPreco* Quantidade;}publicvoidAdicionarProduto(intquantidade) {Quantidade += quantidade;}publicvoidRemoverProdutos(intquantidade) {Quantidade = Quantidade -quantidade;}publicoverridestringToString() {returnNome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);}}
+          }
